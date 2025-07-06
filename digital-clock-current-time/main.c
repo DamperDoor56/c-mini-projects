8bd51b7ecbf6ceb19d14305dbdac939f68083750
Ludmila Lopez <ludmit@outlook.com>
2025-07-06 18:53:51 -0300

    feat: 🕐 show current time!

--- digital-clock-current-time/main.c
+++ digital-clock-current-time/main.c
@@ -1,4 +1,17 @@
 #include<stdio.h>
+#include<time.h>
+
 
 int main() {
+    time_t s;
+    struct tm* current_time;
+    struct tm* ;
+
+    // time in seconds
+    s = time(NULL);
+    // get current time owo
+
+    current_time = localtime(&s);
+
+    printf("%02d : %02d : %02d", current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
 }
